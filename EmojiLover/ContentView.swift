3bad69c929b5f3ffc41237c3b265957a6cab138b
nikos0406 <107640882+nikos0406@users.noreply.github.com>
2024-02-13 18:23:54 +0100

    Just some code to test basic functionalities.

--- EmojiLover/ContentView.swift
+++ EmojiLover/ContentView.swift
@@ -7,15 +7,30 @@
 
 import SwiftUI
 
+enum Emoji: String, CaseIterable{
+    case 🧬, 👹, 👾, 👻
+}
+
 struct ContentView: View {
+    @State var selection: Emoji = .👾
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView{
+            VStack{
+                Text(selection.rawValue)
+                    .font(.system(size: 150))
+                
+                // Dropdown Menu
+                Picker("Select Emoji", selection: $selection){
+                    ForEach(Emoji.allCases, id: \.self){
+                        emoji in Text(emoji.rawValue)
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            .navigationTitle("Emoji Lovers!")
+            .padding()
         }
-        .padding()
     }
 }
 
